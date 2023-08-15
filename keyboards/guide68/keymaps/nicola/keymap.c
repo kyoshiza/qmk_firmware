c0f28d8593f3aed0deaa071c2080df4149532d34
Kenji Yoshizawa <105102132+kyoshiza@users.noreply.github.com>
2023-08-15 16:24:41 +0900

    Add files via upload

--- keyboards/guide68/keymaps/nicola/keymap.c
+++ keyboards/guide68/keymaps/nicola/keymap.c
@@ -54,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ├──────┴┬──┴┬──┴┬──┴┬──┴┬──┴┐  └┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬───┬───┤
      * │ LShift       │  Z   │  X   │  C   │  V   │  B   │    │  N   │  M   │  ,   │  .   │  /   │RCtrl │  ↑  │RShift│
      * ├────┬──┴─┬─┴──┬┴───┴┬──┴──┬┘  ┌┴───┴┬──┴─┬─┴──┬┴───┼───┼───┼───┤
-     * │ L1     │  LGUI  │  LAlt  │   Spc    │   Spc    │    │   Spc    │  Spc   │  RAlt  │  RGUI  │  ←  │  ↓  │  →  │
+     * │ L1     │  LGUI  │  LAlt  │   Spc    │   Spc    │    │   Spc    │  Spc   │  RAlt  │   L1   │  ←  │  ↓  │  →  │
      * └────┴────┴────┴─────┴─────┘    └─────┴────┴────┴────┴───┴───┴───┘
      */
   [_QWERTY] = LAYOUT( \
@@ -62,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,   KC_Q,    KC_W,    KC_E,  KC_R,    KC_T,      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,      \
       KC_LCTL,   KC_A,    KC_S,    KC_D,  KC_F,    KC_G,      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,      \
       KC_LSFT,    KC_Z,    KC_X,    KC_C,  KC_V,    KC_B,      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RCTL, KC_UP, KC_RSFT,      \
-      MO(_FUNC),    KC_LGUI,   KC_LALT,        KC_EISU,  KC_SPC,  KC_SPC, KC_KANA2,         KC_RALT, KC_RGUI, KC_LEFT,KC_DOWN,KC_RGHT),
+      MO(_FUNC),    KC_LGUI,   KC_LALT,        KC_EISU,  KC_SPC,  KC_SPC, KC_KANA2,         KC_RALT,MO(_FUNC), KC_LEFT,KC_DOWN,KC_RGHT),
     
 // NICOLA親指シフト
   // デフォルトレイヤーに関係なくQWERTYで
@@ -71,16 +71,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,  NG_Q,    NG_W,    NG_E,    NG_R,   NG_T,     NG_Y,    NG_U,   NG_I,    NG_O,    NG_P,    NG_LBRC,  NG_RBRC, NG_BSLS, \
       KC_LCTL, NG_A,    NG_S,    NG_D,    NG_F,   NG_G,     NG_H,    NG_J,   NG_K,    NG_L,    NG_SCLN, NG_QUOT,  KC_ENT,           \
       KC_LSFT, NG_Z,    NG_X,    NG_C,    NG_V,   NG_B,     NG_N,    NG_M,   NG_COMM, NG_DOT,  NG_SLSH, KC_LCTL,  KC_UP,   KC_RSFT, \
-      MO(_FUNC), KC_LGUI, KC_LALT,               KC_TRNS, NG_SHFTL, NG_SHFTR, KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS),
+      MO(_FUNC), KC_LGUI, KC_LALT,               KC_TRNS, NG_SHFTL, NG_SHFTR, KC_TRNS,       KC_TRNS, MO(_FUNC), KC_TRNS,  KC_TRNS, KC_TRNS),
 
 
   // Function Layer
   [_FUNC] = LAYOUT( \
       KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,    KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,KC_TRNS,KC_DEL,\
-      KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_PSCR, KC_TRNS, KC_BRK, KC_TRNS,     \
-      KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,KC_INS,  KC_DEL,  KC_ENT,             \
-      KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,KC_PGUP, KC_TRNS,   \
-      MO(_FUNC),   KC_TRNS,  KC_TRNS,         KC_SPC,  KC_EISU,  KC_KANA2, KC_SPC,         KC_TRNS, KC_TRNS, KC_HOME,KC_PGDN, KC_END)
+      KC_TRNS, KC_TRNS,RGB_M_TW, KC_TRNS, RGB_M_R, RGB_TOG,   KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_PSCR, KC_TRNS, KC_BRK, KC_TRNS,     \
+      KC_TRNS, RGB_M_T, RGB_M_SW, KC_TRNS, RGB_M_P,  RGB_M_G,  KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,KC_INS,  KC_DEL,  KC_ENT,             \
+      KC_TRNS,LCTL(LALT(KC_DEL)),RGB_M_X,KC_TRNS,KC_TRNS,RGB_M_B,RGB_M_SN, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,KC_PGUP, KC_TRNS,   \
+      MO(_FUNC),   KC_TRNS,  KC_TRNS,         KC_SPC,  KC_EISU,  KC_KANA2, KC_SPC,         KC_TRNS,MO(_FUNC), KC_HOME,KC_PGDN, KC_END)
 };
 
 
@@ -90,40 +90,42 @@ void matrix_init_user(void) {
   // NICOLA親指シフト
 }
 
-#ifdef RGBLIGHT_LAYERS
-const rgblight_segment_t PROGMEM rgb_layer_1st[] = RGBLIGHT_LAYER_SEGMENTS(
-    {0, 12, HSV_BLUE}, // 開始LEDインデックス, 連続する個数, 光らせる色(ここではプリセットを使用)
-);
-const rgblight_segment_t PROGMEM rgb_layer_2nd[] = RGBLIGHT_LAYER_SEGMENTS(
-    {0, 12, HSV_GREEN}, // 開始LEDインデックス, 連続する個数, 光らせる色(ここではプリセットを使用)
-);
-const rgblight_segment_t PROGMEM rgb_layer_3rd[] = RGBLIGHT_LAYER_SEGMENTS
-    {0, 12, HSV_RED}, // 開始LEDインデックス, 連続する個数, 光らせる色(ここではプリセットを使用)
-);
-const rgblight_segment_t* const PROGMEM rgb_layers[] = RGBLIGHT_LAYERS_LIST(
+// 開始LEDインデックス, 連続する個数, 光らせる色(ここではプリセットを使用)
+#ifndef RGBLIGHT_LAYERS
+  const rgblight_segment_t PROGMEM rgb_layer_1st[] = RGBLIGHT_LAYER_SEGMENTS(
+    {0, 12, HSV_BLUE}, 
+  );
+  const rgblight_segment_t PROGMEM rgb_layer_2nd[] = RGBLIGHT_LAYER_SEGMENTS(
+    {0, 12, HSV_GREEN}, 
+  );
+  const rgblight_segment_t PROGMEM rgb_layer_3rd[] = RGBLIGHT_LAYER_SEGMENTS(
+    {0, 12, HSV_RED}, 
+  );
+  const rgblight_segment_t* const PROGMEM rgb_layers[] = RGBLIGHT_LAYERS_LIST(
     rgb_layer_1st,
     rgb_layer_2nd,
     rgb_layer_3rd,
-);
+  );
+  
 
-void keyboard_post_init_user(void) {
+  void keyboard_post_init_user(void) {
     rgblight_layers = rgb_layers;
-}
-
-layer_state_t layer_state_set_user(layer_state_t state) {
+  }
+  
+  layer_state_t layer_state_set_user(layer_state_t state) {
     rgblight_set_layer_state(0, layer_state_cmp(state, _QWERTY));
     rgblight_set_layer_state(1, layer_state_cmp(state, _NICOLA));
     rgblight_set_layer_state(2, layer_state_cmp(state, _FUNC));
+    nicola_off();
     return state;
-}
+  }
 #endif // RGBLIGHT_LAYERS
 
 // Initialize the 13th LED
 void keyboard_post_init_user(void) {
     for(int i=0; i<RGBLED_NUM; ++i) {
-        rgblight_setrgb_at(0, 0, 0, i);
+	rgblight_setrgb_at(43, 255, 40, i);  // YELLOW (NICOLA off)
     }
-    rgblight_sethsv_at(170,255,40, RGBLED_NUM-1); // the last LED = BLUE (NICOLA off)
     nicola_off();
 }
 
@@ -138,8 +140,10 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 //        send_string(SS_TAP(X_LANG2)); // Mac
         nicola_off();
         // NICOLA親指シフト
-        rgblight_sethsv_at(170,255,40, RGBLED_NUM-1); // the last LED = BLUE (NICOLA off)
-
+      	for(int i=0; i<RGBLED_NUM; ++i) {
+		rgblight_setrgb_at(0, 0, 64, i);  // BLUE
+      	}
+//        rgblight_sethsv_at(170,255,40, RGBLED_NUM-1); // the last LED = BLUE (NICOLA off)
       }
       return false;
       break;
@@ -151,7 +155,10 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 //        send_string(SS_TAP(X_LANG1)); // Mac
         nicola_on();
         // NICOLA親指シフト
-        rgblight_sethsv_at(85,255,40, RGBLED_NUM-1); // the last LED = GREEN (NICOLA on)
+	for(int i=0; i<RGBLED_NUM; ++i) {
+		rgblight_setrgb_at(0, 64, 0, i);  // GREEN
+	}
+//        rgblight_sethsv_at(85,255,40, RGBLED_NUM-1); // the last LED = GREEN (NICOLA on)
 
       }
       return false;
