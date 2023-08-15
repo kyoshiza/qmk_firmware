185b8727510e6f21f69bdacf1be78aeaf2bebb9d
Kenji Yoshizawa <105102132+kyoshiza@users.noreply.github.com>
2023-08-15 16:28:37 +0900

    Add files via upload

--- keyboards/guide68/config.h
+++ keyboards/guide68/config.h
@@ -46,11 +46,14 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* ws2812 RGB LED */
 #define RGB_DI_PIN D3
 
-#ifndef RGBLED_NUM
+#define RGBLIGHT_LAYERS /* enable RGBLIGHT function */
+#undef RGBLED_NUM
+/*  #ifndef RGBLED_NUM  */
   #define RGBLED_NUM 12
-  #define RGBLIGHT_SPLIT
   #define RGBLED_SPLIT { 6, 6 }
-#endif
+//  #define RGBLIGHT_SPLIT
+  #define RGBLIGHT_MAX_LAYERS 3 /* maximum up to 32 */
+/*  #endif  */
 
 #define RGBLIGHT_EFFECT_BREATHING
 #define RGBLIGHT_EFFECT_RAINBOW_MOOD
@@ -63,6 +66,37 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGBLIGHT_EFFECT_ALTERNATING
 #define RGBLIGHT_EFFECT_TWINKLE
 
+/* 
+#define LED_LAYOUT( \
+    L00, L01, L02, L03, L04, L05,      R00, R01, R02, R03, R04, R05, R06, R07, R17, \
+    L10, L11, L12, L13, L14, L15,      R10, R11, R12, R13, R14, R15, R16, R27, \
+    L20, L21, L22, L23, L24, L25,      R20, R21, R22, R23, R24, R25, R26, \
+    L30, L31, L32, L33, L34, L35,      R30, R31, R32, R33, R34, R35, R36, R37, \
+    L40, L41, L42, L43, L44,           R40, R41, R43, R44, R45, R46, R47 \
+  ) \
+  { \
+    { L00,   L01,   L02,   L03,   L04,   L05,   KC_NO, KC_NO }, \
+    { L10,   L11,   L12,   L13,   L14,   L15,   KC_NO, KC_NO }, \
+    { L20,   L21,   L22,   L23,   L24,   L25,   KC_NO, KC_NO }, \
+    { L30,   L31,   L32,   L33,   L34,   L35,   KC_NO, KC_NO }, \
+    { L40,   L41,   L42,   L43,   L44,   KC_NO, KC_NO, KC_NO }, \
+    { R00,   R01,   R02,   R03,   R04,   R05,   R06,   R07   }, \
+    { R10,   R11,   R12,   R13,   R14,   R15,   R16,   R17   }, \
+    { R20,   R21,   R22,   R23,   R24,   R25,   R26,   R27   }, \
+    { R30,   R31,   R32,   R33,   R34,   R35,   R36,   R37   }, \
+    { R40,   R41,   KC_NO, R43,   R44,   R45,   R46,   R47   }  \
+  }
+
+#define RGBLIGHT_LED_MAP LED_LAYOUT( \
+      5,  6, \
+      4,  7, \
+      3,  8, \
+      2,  9, \
+  0,  1,  10,  11)
+#define RGBLIGHT_LED_MAP { 0, 1 , 2, 3, 4, 5, \
+                          11, 10, 9, 8, 7, 6 }
+*/
+
 #ifndef IOS_DEVICE_ENABLE
   #define RGBLIGHT_LIMIT_VAL 180
   #define RGBLIGHT_VAL_STEP 17
@@ -70,7 +104,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
   #define RGBLIGHT_LIMIT_VAL 50
   #define RGBLIGHT_VAL_STEP 4
 #endif
-#define RGBLIGHT_HUE_STEP 10
+#define RGBLIGHT_HUE_STEP 169
+// #define RGBLIGHT_HUE_STEP 10
 #define RGBLIGHT_SAT_STEP 17
 
 #if defined(RGBLIGHT_ENABLE) && !defined(IOS_DEVICE_ENABLE)
--- keyboards/guide68/rules.mk
+++ keyboards/guide68/rules.mk
@@ -12,11 +12,12 @@ BOOTLOADER = caterina
 # EXTRAKEY_ENABLE = yes       # Audio control and System control
 # CONSOLE_ENABLE = no          # Console for debug
 # COMMAND_ENABLE = no        # Commands for debug and configuration
-# NKRO_ENABLE = no                # Enable N-Key Rollover
+# NKRO_ENABLE = yes                # Enable N-Key Rollover
 # BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 # AUDIO_ENABLE = no               # Audio output
 # RGBLIGHT_ENABLE = yes        # Enable WS2812 RGB underlight.
-IOS_DEVICE_ENABLE = yes      # Enabe iPhone to connect the keyboard
+# RGBLIGHT_SLEEP = yes
+# IOS_DEVICE_ENABLE = yes      # Enabe iPhone to connect the keyboard
 # SPLIT_KEYBOARD = yes
 
 DEFAULT_FOLDER = guide68
